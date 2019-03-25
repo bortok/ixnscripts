b7d374622eec013e32ffea0b492ccb9fde202044
Alex Bortok <dev@bortok.com>
2019-03-24 23:04:05 -0700

    Config initialization

--- PacketLossDuration_csv.py
+++ PacketLossDuration_csv.py
@@ -6,9 +6,12 @@
 #    python <script>.py linux
 
 import sys, os, traceback
-import requests, json
+import json
 import time, datetime
 import csv
+
+import requests
+requests.packages.urllib3.disable_warnings()
 
 from libixnscripts import *
 
@@ -39,6 +42,101 @@ if len(sys.argv) > 4:
         sys.exit("\nError: %s is not a known option. Choices are 'windows', 'windowsConnectionMgr or 'linux'." % sys.argv[4])
     osPlatform = sys.argv[4]
 
+#Test Functions
+
+def startProto():
+#from IxNetRestApiProtocol import Protocol
+#    protocolObj = Protocol(mainObj)
+#    protocolObj.startAllProtocols()
+#    protocolObj.verifyArp(ipType='ipv4')
+#    protocolObj.verifyProtocolSessionsUp(protocolViewName='BGP Peer Per Port', timeout=120)
+
+    response = requests.post(url + '/ixnetwork/operations/startallprotocols', data=json.dumps({}), headers={'content-type': 'application/json'}, verify=False)
+    time.sleep(5)
+    response.json()['state']
+    return 0
+
+def stopProto():
+#from IxNetRestApiProtocol import Protocol
+# protocolObj.stopAllProtocols()
+    response = requests.post(url + '/ixnetwork/operations/stopallprotocols', data=json.dumps({}), headers={'content-type': 'application/json'}, verify=False)
+    time.sleep(5)
+    response.json()['state']
+    return 0
+
+def startTraffic():
+#from IxNetRestApiTraffic import Traffic
+#    trafficObj = Traffic(mainObj)
+#    trafficObj.startTraffic(regenerateTraffic=True, applyTraffic=True)
+    
+    requests.post(url + '/ixnetwork/traffic/operations/generate', data=json.dumps({'arg1': url + '/ixnetwork/traffic/1'}), headers={'content-type': 'application/json'}, verify=False)
+    response = requests.post(url + '/ixnetwork/traffic/operations/apply', data=json.dumps({'arg1': '/ixnetwork/traffic'}), headers={'content-type': 'application/json'}, verify=False)
+    response = requests.post(url + '/ixnetwork/traffic/operations/start', data=json.dumps({'arg1': '/ixnetwork/traffic'}), headers={'content-type': 'application/json'}, verify=False)
+    response.json()
+    return 0
+
+def stopTraffic():
+#from IxNetRestApiTraffic import Traffic
+#    trafficObj.stopTraffic()
+    response = requests.post(url + '/ixnetwork/traffic/operations/stop', data=json.dumps({'arg1': '/ixnetwork/traffic'}), headers={'content-type': 'application/json'}, verify=False)
+    response.json()
+    return 0
+
+ 
+def clearStats():
+#from IxNetRestApiStatistics import Statistics
+#    statObj = Statistics(mainObj)
+#    stats = statObj.clearStats()
+    response = requests.post(url + '/ixnetwork/operations/clearstats', headers=jsonHeader, verify=False)
+    if response.status_code != 202:
+        print('something went wrong')
+
+def getStats(statName):
+#    stats = statObj.getStats(viewName='Flow Statistics')    
+    #Parse Through list of returned views and then pick the one that matches the caption to be used
+    response = requests.get(url + '/ixnetwork/statistics/view', headers=jsonHeader, verify=False)
+    for i in response.json():
+        if (i['caption']) == 'Traffic Item Statistics':
+            viewNum = i['id']
+    if viewNum is None:
+        print("Error getting response from the API server")
+        sys.exit()
+    view = ('%s%s%s%s' % (url, '/ixnetwork/statistics/view/', viewNum, '/page'))
+    response = requests.get(view, headers=jsonHeader, verify=False)
+    return response
+
+def getPackLossDuration():
+    view=getStats('Traffic Item Statistics')
+    loss_duration_column = view.json()['columnCaptions'].index("Packet Loss Duration (ms)")
+    # DEBUG
+    #print("DEBUG STATS JSON\n%s" % (view.json()))
+    #print("DEBUG LOSS DURATION INDEX\n%d" % (loss_duration_column))
+    value_str = view.json()['pageValues'][0][0][loss_duration_column]
+    print("Packet loss duration: %s ms" % (value_str))
+    if isFloat(value_str):
+        value = float(value_str)
+    else:
+        value = 0.0
+    return value
+
+def startProtoAndTraffic():
+    stopProto()
+    print('Starting protocol and traffic within 5 seconds...')
+    startProto()
+    time.sleep(5)
+    startTraffic()
+
+def stopProtoAndTraffic():
+    print('Stopping traffic. Wait...')
+    stopTraffic()
+    stopProto()
+    print('stopped')
+
+# Parse arguments
+if len(sys.argv) < 4:
+    print("Usage: %s api_host api_session_id api_tcp_port [api_platform]" % (sys.argv[0]))
+    sys.exit()
+
 
 try:
     #---------- Preference Settings --------------
@@ -47,10 +145,10 @@ try:
     enableDebugTracing = True
     deleteSessionAfterTest = False ;# For Windows Connection Mgr and Linux API server only
 
-#    licenseServerIp = '10.36.237.207'
-#    licenseModel = 'perpetual'
+    licenseServerIp = '10.36.237.207'
+    licenseModel = 'perpetual'
 
-    ixChassisIp = api_host
+    ixChassisIp = '10.36.237.207'
     # [chassisIp, cardNumber, slotNumber]
     portList = [[ixChassisIp, '2', '9'], [ixChassisIp, '2', '10']]
 
@@ -74,6 +172,77 @@ try:
                           )
         
     #---------- Preference Settings End --------------
+    # Only need to blank the config for Windows because osPlatforms such as Linux and
+    # Windows Connection Mgr supports multiple sessions and a new session always come up as a blank config.
+    if osPlatform == 'windows':
+        mainObj.newBlankConfig() 
+
+    mainObj.configLicenseServerDetails([licenseServerIp], licenseModel)
+
+    portObj = PortMgmt(mainObj)
+    portObj.assignPorts(portList, forceTakePortOwnership)
+
+    protocolObj = Protocol(mainObj)
+    topologyObj1 = protocolObj.createTopologyNgpf(portList=[portList[0]],
+                                              topologyName='Topo1')
+
+    deviceGroupObj1 = protocolObj.createDeviceGroupNgpf(topologyObj1,
+                                                    multiplier=1,
+                                                    deviceGroupName='DG1')
+
+    topologyObj2 = protocolObj.createTopologyNgpf(portList=[portList[1]],
+                                              topologyName='Topo2')
+
+    deviceGroupObj2 = protocolObj.createDeviceGroupNgpf(topologyObj2,
+                                                    multiplier=1,
+                                                    deviceGroupName='DG2')
+
+    ethernetObj1 = protocolObj.configEthernetNgpf(deviceGroupObj1,
+                                                  ethernetName='MyEth1',
+                                                  macAddress={'start': '00:01:01:00:00:01',
+                                                              'direction': 'increment',
+                                                              'step': '00:00:00:00:00:01'},
+                                                  macAddressPortStep='disabled',
+                                                  vlanId={'start': 103,
+                                                          'direction': 'increment',
+                                                          'step':0})
+
+    ethernetObj2 = protocolObj.configEthernetNgpf(deviceGroupObj2,
+                                                  ethernetName='MyEth2',
+                                                  macAddress={'start': '00:01:02:00:00:01',
+                                                              'direction': 'increment',
+                                                              'step': '00:00:00:00:00:01'},
+                                                  macAddressPortStep='disabled',
+                                                  vlanId={'start': 103,
+                                                          'direction': 'increment',
+                                                          'step':0})
+    ipv4Obj1 = protocolObj.configIpv4Ngpf(ethernetObj1,
+                                          ipv4Address={'start': '1.1.1.1',
+                                                       'direction': 'increment',
+                                                       'step': '0.0.0.1'},
+                                          ipv4AddressPortStep='disabled',
+                                          gateway={'start': '1.1.1.2',
+                                                   'direction': 'increment',
+                                                   'step': '0.0.0.0'},
+                                          gatewayPortStep='disabled',
+                                          prefix=24,
+                                          resolveGateway=True)
+
+    ipv4Obj2 = protocolObj.configIpv4Ngpf(ethernetObj2,
+                                          ipv4Address={'start': '1.1.1.2',
+                                                       'direction': 'increment',
+                                                       'step': '0.0.0.1'},
+                                          ipv4AddressPortStep='disabled',
+                                          gateway={'start': '1.1.1.1',
+                                                   'direction': 'increment',
+                                                   'step': '0.0.0.0'},
+                                          gatewayPortStep='disabled',
+                                          prefix=24,
+                                          resolveGateway=True)
+
+    protocolObj.startAllProtocols()
+    protocolObj.verifyProtocolSessionsUp()
+
 except (IxNetRestApiException, Exception, KeyboardInterrupt):
     if enableDebugTracing:
         if not bool(re.search('ConnectionError', traceback.format_exc())):
@@ -105,7 +274,6 @@ test_iterration = 0
 
 user_input = input("Start traffic? (Anything other than 'y' will kill it) ")
 if user_input == "y":
-    requests.packages.urllib3.disable_warnings()
     startProtoAndTraffic()
     today = datetime.datetime.today()
     csv_filename = today.strftime('packet_loss_tests_%Y_%m%d_%H%M.csv')
