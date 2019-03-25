a06dc630c97177f039b375458f035f80f093538b
Alex Bortok <dev@bortok.com>
2019-03-25 10:52:40 -0700

    Getting stats using IxNetRestApiStatistics

--- PacketLossDuration_csv.py
+++ PacketLossDuration_csv.py
@@ -46,35 +46,12 @@ if len(sys.argv) > 4:
 
 #Test Functions
 
-def startProto():
-#from IxNetRestApiProtocol import Protocol
-#    protocolObj = Protocol(mainObj)
-#    protocolObj.startAllProtocols()
-#    protocolObj.verifyArp(ipType='ipv4')
-#    protocolObj.verifyProtocolSessionsUp(protocolViewName='BGP Peer Per Port', timeout=120)
-
-    response = requests.post(url + '/ixnetwork/operations/startallprotocols', data=json.dumps({}), headers={'content-type': 'application/json'}, verify=False)
-    time.sleep(5)
-    response.json()['state']
-    return 0
-
 def stopProto():
 #from IxNetRestApiProtocol import Protocol
 # protocolObj.stopAllProtocols()
     response = requests.post(url + '/ixnetwork/operations/stopallprotocols', data=json.dumps({}), headers={'content-type': 'application/json'}, verify=False)
     time.sleep(5)
     response.json()['state']
-    return 0
-
-def startTraffic():
-#from IxNetRestApiTraffic import Traffic
-#    trafficObj = Traffic(mainObj)
-#    trafficObj.startTraffic(regenerateTraffic=True, applyTraffic=True)
-    
-    requests.post(url + '/ixnetwork/traffic/operations/generate', data=json.dumps({'arg1': url + '/ixnetwork/traffic/1'}), headers={'content-type': 'application/json'}, verify=False)
-    response = requests.post(url + '/ixnetwork/traffic/operations/apply', data=json.dumps({'arg1': '/ixnetwork/traffic'}), headers={'content-type': 'application/json'}, verify=False)
-    response = requests.post(url + '/ixnetwork/traffic/operations/start', data=json.dumps({'arg1': '/ixnetwork/traffic'}), headers={'content-type': 'application/json'}, verify=False)
-    response.json()
     return 0
 
 def stopTraffic():
@@ -93,40 +70,17 @@ def clearStats():
     if response.status_code != 202:
         print('something went wrong')
 
-def getStats(statName):
-#    stats = statObj.getStats(viewName='Flow Statistics')    
-    #Parse Through list of returned views and then pick the one that matches the caption to be used
-    response = requests.get(url + '/ixnetwork/statistics/view', headers=jsonHeader, verify=False)
-    for i in response.json():
-        if (i['caption']) == 'Traffic Item Statistics':
-            viewNum = i['id']
-    if viewNum is None:
-        print("Error getting response from the API server")
-        sys.exit()
-    view = ('%s%s%s%s' % (url, '/ixnetwork/statistics/view/', viewNum, '/page'))
-    response = requests.get(view, headers=jsonHeader, verify=False)
-    return response
-
 def getPackLossDuration():
-    view=getStats('Traffic Item Statistics')
-    loss_duration_column = view.json()['columnCaptions'].index("Packet Loss Duration (ms)")
+    stats = statObj.getStats(viewName='Traffic Item Statistics')    
     # DEBUG
-    #print("DEBUG STATS JSON\n%s" % (view.json()))
-    #print("DEBUG LOSS DURATION INDEX\n%d" % (loss_duration_column))
-    value_str = view.json()['pageValues'][0][0][loss_duration_column]
+    print("DEBUG STATS JSON\n%s" % (stats))
+    value_str = stats[1]["Packet Loss Duration (ms)"]
     print("Packet loss duration: %s ms" % (value_str))
     if isFloat(value_str):
         value = float(value_str)
     else:
         value = 0.0
     return value
-
-def startProtoAndTraffic():
-    stopProto()
-    print('Starting protocol and traffic within 5 seconds...')
-    startProto()
-    time.sleep(5)
-    startTraffic()
 
 def stopProtoAndTraffic():
     print('Stopping traffic. Wait...')
@@ -264,25 +218,16 @@ try:
                                                  configElements = [{'transmissionType': 'continuous',
                                                                     'frameRate': 10,
                                                                     'frameRateType': 'percentLineRate',
-                                                                    'frameSize': 128}])
+                                                                    'frameSize': 512}])
     
     trafficItemObj   = trafficStatus[0]
     endpointObj      = trafficStatus[1][0]
     configElementObj = trafficStatus[2][0]
+    
+    # Enable tracking of packet loss duration
+    trafficObj.enablePacketLossDuration()
 
-    # Example on how to modify Traffic Item
-    trafficObj.configTrafficItem(mode='modify',
-                                 obj=trafficItemObj,
-                                 trafficItem = {'name':'Topo1_mod_Topo2'})
-    
-    trafficObj.configTrafficItem(mode='modify',
-                                 obj=configElementObj,
-                                 configElements = {'frameSize':'512'})
-    
-    trafficObj.configTrafficItem(mode='modify',
-                                 obj=endpointObj,
-                                 endpoints = {'name':'Flow-Group-10'})
-    
+    # Start traffic
     trafficObj.startTraffic(regenerateTraffic=True, applyTraffic=True)
 
 
@@ -294,22 +239,6 @@ try:
     trafficObj.checkTrafficState(expectedState=['started'], timeout=45)
 
     statObj = Statistics(mainObj)
-    stats = statObj.getStats(viewName='Flow Statistics')
-
-    print('\n{txPort:10} {txFrames:15} {rxPort:10} {rxFrames:15} {frameLoss:10}'.format(
-        txPort='txPort', txFrames='txFrames', rxPort='rxPort', rxFrames='rxFrames', frameLoss='frameLoss'))
-    print('-'*90)
-
-    for flowGroup,values in stats.items():
-        txPort = values['Tx Port']
-        rxPort = values['Rx Port']
-        txFrames = values['Tx Frames']
-        rxFrames = values['Rx Frames']
-        frameLoss = values['Frames Delta']
-
-        print('{txPort:10} {txFrames:15} {rxPort:10} {rxFrames:15} {frameLoss:10} '.format(
-            txPort=txPort, txFrames=txFrames, rxPort=rxPort, rxFrames=rxFrames, frameLoss=frameLoss))
-
 
     # Initialize counters
     test_number = 0
